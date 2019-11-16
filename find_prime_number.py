57e887b6747d2072fa6683e0f48378527c12f099
yahoo168 <a86305394@gmail.com>
2019-11-16 13:29:16 +0800

    Create find_prime_number.py

---
+++ find_prime_number.py
@@ -0,0 +1,7 @@
+def is_prime(n):
+    """判断素数的函数"""
+    assert n > 0
+    for factor in range(2, int(sqrt(n)) + 1):
+        if n % factor == 0:
+            return False
+    return True if n != 1 else False
